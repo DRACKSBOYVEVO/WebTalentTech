4172aae16894cc2b50cd77cb0480c317c322eaf8
Administrador <Alkabiz@hotmail.com>
2024-12-08 14:33:00 -0500

    feat: se actualizaron propiedades de los modelos LiderSocial y Voto

--- Models/LiderSocial.cs
+++ Models/LiderSocial.cs
@@ -8,9 +8,15 @@ public class LiderSocial
     [Key]
     public int LiderSocialId { get; set; }
     [Required]
+
     public string Nombre { get; set; }
     [Required]
+
     public int BarrioId { get; set; }
     [ForeignKey("BarrioId")]
     public Barrio Barrio { get; set; }
+
+    public int CiudadId { get; set; }
+    [ForeignKey("CiudadId")]
+    public Ciudad Ciudad { get; set; }
 }
--- Models/Voto.cs
+++ Models/Voto.cs
@@ -8,13 +8,26 @@ public class Voto
     [Key]
     public int VotoId { get; set; }
     [Required]
+
     public int EncuestaId { get; set; }
     [ForeignKey("EncuestaId")]
+
     public Encuesta Encuesta { get; set; }
     [Required]
+
     public int LiderSocialId { get; set; }
     [ForeignKey("LiderSocialId")]
+
     public LiderSocial LiderSocial { get; set; }
     [Required]
+
     public DateTime FechaVoto { get; set; }
+    [Required]
+
+    public string Cedula { get; set; }
+    [Required]
+
+    public int ProyectoSocialId { get; set; }
+    [ForeignKey("ProyectoSocialId")]
+    public ProyectoSocial ProyectoSocial { get; set; }
 }
