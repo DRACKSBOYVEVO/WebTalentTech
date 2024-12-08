998e9601c447f5e8c6538280ff284642752da5fc
Administrador <Alkabiz@hotmail.com>
2024-12-08 14:34:10 -0500

    refact: constructor principal, y remover acción no utilizada

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,21 +4,11 @@ using Web.Models;
 
 namespace Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> logger) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger<HomeController> _logger = logger;
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Votar()
         {
             return View();
         }
