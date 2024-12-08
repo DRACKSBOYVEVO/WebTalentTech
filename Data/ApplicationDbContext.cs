2b04e25a2915ae558ce18a8dde2d86189abcc9a9
Administrador <Alkabiz@hotmail.com>
2024-12-08 14:41:21 -0500

    refact: referencia a los nuevos modelos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,11 +11,14 @@ namespace Web.Data
         {
         }
 
-        public DbSet<Barrio> Barrios { get; set; }
+        public DbSet<Ciudad> Ciudades { get; set; }
         public DbSet<Comuna> Comunas { get; set; }
+        public DbSet<Barrio> Barrios { get; set; }
         public DbSet<Departamento> Departamentos { get; set; }
-        public DbSet<Encuesta> Encuestas { get; set; }
+
+        public DbSet<ProyectoSocial> ProyectoSociales { get; set; }
         public DbSet<LiderSocial> LiderSocials { get; set; }
+        public DbSet<Encuesta> Encuestas { get; set; }
         public DbSet<Voto> Votos { get; set; }
     }
 }
