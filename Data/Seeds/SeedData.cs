f6c8f1b0fb132b75d245d7dae336a12f9fb29467
Administrador <Alkabiz@hotmail.com>
2024-12-10 15:58:35 -0500

    feat: Crear la clase SeedData con la lógica para inicializar roles predeterminados

---
+++ Data/Seeds/SeedData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Web.Data.Seeds;
+
+public class SeedData
+{
+    public static async Task InitializeRolesAsync(RoleManager<IdentityRole> roleManager)
+    {
+        string[] roles = ["Líder Social", "Junta de Acción Comunal", "Delegado Alcaldía"];
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+    }
+
+}
