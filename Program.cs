1c48f4b0d0d137525404a369560758d4af9ad2df
Administrador <Alkabiz@hotmail.com>
2024-12-11 18:28:23 -0500

    feat: se actualizaron los servicios en el archivo Program.cs para respetar la separación de capas

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
-using Web.Data;
-using Web.Data.Seeds;
+using TuVozLocal.BusinessLogic.Services;
+using TuVozLocal.DataAccess.Context;
+using TuVozLocal.DataAccess.Seeds;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+var connectionString = builder.Configuration.GetConnectionString("AzureSQLConnection")
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
+
+//Services Add
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -41,7 +50,7 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
-// Lógica de inicialización de roles
+//Lógica de inicialización de roles
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
